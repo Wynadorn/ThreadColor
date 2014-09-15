86722ccb916aa0e41948e8878a1e9c70f574025e
Wynadorn <JanosdeVries@Gmail.com>
2014-09-15 13:49:34 +0200

    Issue #15 seems to be fixed

--- ThreadCollor/MainForm.cs
+++ ThreadCollor/MainForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -41,6 +42,7 @@ namespace ThreadCollor
 
             //Initialize the form
             InitializeComponent();
+            Thread.CurrentThread.Priority = ThreadPriority.AboveNormal;
         }
 
         /// <summary>
